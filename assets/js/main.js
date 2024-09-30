61c0d389bb84c3320c999caa68149feac25ebb68
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-09-30 14:18:23 +0200

    add three variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
 console.log("Hello world");
+
+// Prima fase: preparazione 
+// Creo la prima variabile dove salvare il numero di chilometri
+let num_of_km;
+
+// Creo la seconda variabile dove salvare l'età del passeggiero
+let passenger_age;
+
+// Creo la variabile message dove salvare il messaggio finale
+let message
+
+
+// Seconda fase: raccogliere dati
+// Utilizzo un prompt per salvare la prima parola
+
+// Utilizzo un prompt per salvare la seconda parola
+
+// stampo le due parole
+
+
+// Terza fase: elaborazione codice
+// verifico se la prima parola è più lunga della seconda
+
+// verifico se la seconda parola è più lunga
+
+// altrimenti sono uguali
+
+
+// Quarta fase: mostrare output
+// stampo il messaggio in console
